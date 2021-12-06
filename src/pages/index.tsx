7e0ac4b60a3f0cc0119cfdcf3d1f736b67718c98
mahiro miyazaki <mahiro.siawasemono@ezweb.ne.jp>
2021-12-07 01:34:36 +0900

    🎨 不要な部分の削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,8 @@
 import type { NextPage } from 'next';
 import 'tailwindcss/tailwind.css';
-import { client } from '../libs/client';
 import Main from './main';
 
-export const getStaticProps = async () => {
-  const data = await client.get({
-    endpoint: 'test',
-  });
-
-  return {
-    props: {
-      data,
-    }
-  };
-};
-
-const Home: NextPage = (props: any) => {
-  const { data } = props;
-
-  console.log(data);
-
+const Home: NextPage = () => {
   return (
     <div>
       <Main />
