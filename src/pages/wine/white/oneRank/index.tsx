ada4b2e72a4a607602dcf143a638c6abab801c35
mahiro miyazaki <mahiro.siawasemono@ezweb.ne.jp>
2021-12-13 16:55:42 +0900

    ✨ ワイン一覧のスタイリング

--- src/pages/wine/white/oneRank/index.tsx
+++ src/pages/wine/white/oneRank/index.tsx
@@ -1,44 +1,71 @@
 import { client } from 'src/libs/client';
 
-
 export const getStaticProps = async () => {
   const data = await client.get({
     endpoint: `whitewine`,
   });
 
+  const sampleImage = await client.get({
+    endpoint: 'wineimage',
+  });
+
   return {
     props: {
       data,
+      sampleImage,
     },
   };
 };
 
 const OneRank: React.FC = (props: any) => {
   const { data } = props;
+  const { sampleImage } = props;
 
   const keyRank = '1ランク';
   const oneRankData = data.contents?.filter(
     (data: any) => data.rank[0] === keyRank
   );
 
-  // console.log(oneRankData?.image?.url);
-
-  // console.log(oneRankData[2].image.ur);
-  
-
-
   return (
-    <div>
+    <div className="h-screen">
       {oneRankData?.map((data: any) => (
-        <div key={data.id}>
-          {/* <img src={data?.image ? data.image.url : 'image/ikgroup-wineimage.jpg'} alt="ワインの画像です" /> */}
-          <h1>{data.name}</h1>
-          <p>{data.variety}</p>
-          <p>{data.origin}</p>
-          <p>{data.taste}</p>
+        <div
+          className="h-1/3 flex justify-around items-center bg-blue-100 mb-6"
+          key={data.id}
+        >
+          <div>
+            <img
+              className="w-60 h-60 object-cover rounded-lg"
+              src={data?.image ? data.image.url : sampleImage.wineImage.url}
+              alt="ワインの画像です"
+            />
+          </div>
+          <dl className="flex flex-wrap tracking-wide w-1/2">
+            <dt className="leading-relaxed flex w-3/12 font-bold">
+              ワイン名 :{' '}
+            </dt>
+            <dd className="leading-relaxed w-9/12">{data.name}</dd>
+            <dt className="leading-relaxed flex w-3/12 font-bold">品種 : </dt>
+            <dd className="leading-relaxed w-9/12">{data.variety}</dd>
+            <dt className="leading-relaxed flex w-3/12 font-bold">産地 : </dt>
+            <dd className="leading-relaxed w-9/12">{data.origin}</dd>
+            <dt className="leading-relaxed flex w-3/12 font-bold">味わい : </dt>
+            <dd className="leading-relaxed w-9/12">{data.taste}</dd>
+            {data.producer ? (
+              <>
+                <dt className="flex w-3/12 font-bold">生産者 : </dt>
+                <dd className="w-9/12">{data.producer}</dd>
+              </>
+            ) : null}
+            {data.remarks ? (
+              <>
+                <dt className="flex w-3/12 font-bold">備考 : </dt>
+                <dd className="w-9/12">{data.remarks}</dd>
+              </>
+            ) : null}
+          </dl>
         </div>
       ))}
-      <img src="/public/image/ikgroup-wineimage.jpg" alt="aaaaa" />
     </div>
   );
 };
