6c4e17842e59c07487e3b2e8217ae9c2ca3cd55f
mahiro miyazaki <mahiro.siawasemono@ezweb.ne.jp>
2021-12-18 19:19:38 +0900

    ✨ ロゼとスパークのページを作成

---
+++ src/pages/front/rose/index.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from "next";
+
+const Rose: NextPage = () => {
+  return <div>ロゼのページだよ</div>;
+};
+
+export default Rose;
---
+++ src/pages/front/sparkling/index.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from "next";
+
+const Spark: NextPage = () => {
+  return <div>スパークのページだよ</div>;
+};
+
+export default Spark;
