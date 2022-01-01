ab7309855205d8fbb7f2b8abf845e199cb782905
mahiro miyazaki <mahiro.siawasemono@ezweb.ne.jp>
2022-01-01 12:53:09 +0900

    ⚡️ トップに戻るボタンを実装

--- src/layouts/containerLayout.tsx
+++ src/layouts/containerLayout.tsx
@@ -1,9 +1,24 @@
 import { ReactNode } from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
 
 type Props = {
   children: ReactNode;
 };
 
 export const ContainerLayout: React.FC<Props> = ({ children }) => {
-  return <div className="mx-auto h-screen bg-gray-100">{children}</div>;
+  const router = useRouter();
+
+  return (
+    <div className="mx-auto bg-gray-100">
+      {router.pathname !== "/" ? (
+        <div className="">
+          <Link href="/">
+            <a className="font-mono block py-5 text-xl bg-gray-200 text-gray-700 shadow-md text-center">トップに戻る</a>
+          </Link>
+        </div>
+      ) : null}
+      {children}
+    </div>
+  );
 };
