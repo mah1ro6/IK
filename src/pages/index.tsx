d4f7bf90008e71c85e475efab708beb351a8fc54
mahiro miyazaki <mahiro.siawasemono@ezweb.ne.jp>
2022-01-20 17:49:36 +0900

    🗑️ 不要な部分の削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,17 +18,6 @@ const Home: NextPage = () => {
     },
   ];
 
-  // const propsData: PropsData[] = [
-  //   {
-  //     url: 'wine',
-  //     text: 'ワイン',
-  //   },
-  //   {
-  //     url: 'whisky',
-  //     text: 'ウイスキー',
-  //   },
-  // ];
-
   return <Links height="h-1/3" propsData={propsData} />;
 };
 
