e0f8e0475c3326bde69adc39e313d98054336b43
mahiro miyazaki <mahiro.siawasemono@ezweb.ne.jp>
2022-02-01 17:30:45 +0900

    ⚡️ コンテンツ削除の修正

--- server.js
+++ server.js
@@ -1,31 +1,41 @@
-const express = require('express');
-const next = require('next');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const express = require("express");
+const next = require("next");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
-const port = parseInt(process.env.PORT, 10) || 3000
-const dev = process.env.NODE_ENV !== 'production'
-const API_URL = process.env.API_URL || 'http://localhost:8888'
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== "production";
+const pro = process.env.NODE_ENV !== "development";
+const API_URL = process.env.API_URL || "http://localhost:8888";
 
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const app = dev ? next({ dev }) : pro ? next({ pro }) : undefined;
 
-app.prepare().then(() => {
-  const server = express();
+const handle = app.getRequestHandler();
 
-  server.use(
-    '/api',
-    createProxyMiddleware({
-      target: API_URL,
-      changeOrigin: true
-    })
-  );
+app
+  .prepare()
+  .then(() => {
+    const server = express();
 
-  server.all('*', (req, res) => {
-    return handle(req, res)
+    server.use(
+      "/api",
+      createProxyMiddleware({
+        target: API_URL,
+        pathRewrite: {
+          "^/api": "/ik_api/src/main.php",
+        },
+        changeOrigin: true,
+      })
+    );
+
+    server.all("*", (req, res) => {
+      return handle(req, res);
+    });
+
+    server.listen(port, (err) => {
+      if (err) throw err;
+      console.log(`> Ready on localhost:${port} - env ${process.env.NODE_ENV}`);
+    });
+  })
+  .catch((err) => {
+    console.log("エラーが発生しました", err);
   });
-
-  server.listen(port, err => {
-    if (err) throw err
-    console.log(`> Ready on http://localhost:${port}`)
-  });
-});
