a1fe2408fd2732bc02a69dbadadd3139976b8b32
mahiro miyazaki <mahiro.siawasemono@ezweb.ne.jp>
2022-02-07 20:20:46 +0900

    🎨 定数の変更

--- src/util/index.tsx
+++ src/util/index.tsx
@@ -1,7 +1,7 @@
-export const keyOneRank = "1ランク";
-export const keyTwoRank = "2ランク";
-export const keyAnotherRank = "その他";
-export const red = "赤ワイン";
-export const white = "白ワイン";
-export const rose = "ロゼワイン";
-export const spark = "スパークリングワイン";
+export const keyOneRank = "oneRank";
+export const keyTwoRank = "twoRank";
+export const keyAnotherRank = "another";
+export const red = "red";
+export const white = "white";
+export const rose = "rose";
+export const spark = "sparkling";
