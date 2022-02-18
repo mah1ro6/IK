942451d96b14d03d894705a8eb18eca881c19a7f
mahiro miyazaki <mahiro.siawasemono@ezweb.ne.jp>
2022-02-18 19:13:31 +0900

    ⚡️ 発注する際に個数を指定できるよう実装

--- src/components/CellarWineLists/index.tsx
+++ src/components/CellarWineLists/index.tsx
@@ -1,9 +1,11 @@
 import { cellarToFront, handleOnOrder } from "src/method";
 import { Data, Props } from "src/types";
 import { Toaster } from "react-hot-toast";
+import { useEffect, useState } from "react";
 
 export const CellarWineLists: React.FC<Props> = (props) => {
   const contents = props.contents;
+  const [counts, setCounts] = useState<number[]>([]);
 
   if (contents.length === 0) {
     return (
@@ -13,9 +15,27 @@ export const CellarWineLists: React.FC<Props> = (props) => {
     );
   }
 
+  useEffect(() => {
+    for (let i = 0; i < contents.length; i++) {
+      setCounts((count) => [...count, 0]);
+    }
+  }, []);
+
+  const addCount = (index: number) => {
+    setCounts(counts.map((count, i) => (i === index ? count + 1 : count)));
+  };
+
+  const reduceCount = (index: number) => {
+    if (counts[index] > 0) {
+      setCounts(counts.map((count, i) => (i === index ? count - 1 : count)));
+    } else {
+      return;
+    }
+  };
+
   return (
     <div className="h-screen sm:h-full">
-      {contents?.map((data: Data) => (
+      {contents?.map((data: Data, index) => (
         <div
           className="flex items-center justify-around mt-6 h-1/3 bg-blue-100 sm:flex-wrap sm:mx-auto sm:py-8 sm:w-11/12 sm:h-auto sm:rounded-lg"
           key={data.id}
@@ -32,13 +52,33 @@ export const CellarWineLists: React.FC<Props> = (props) => {
             >
               表に出す
             </button>
-            <button
-              className="mt-5 mx-3 py-2 w-1/3 font-mono bg-yellow-100 rounded-lg"
-              onClick={() => handleOnOrder(data.id)}
-            >
-              発注する
-            </button>
-            <Toaster />
+            <div className="flex justify-between mt-5">
+              <div className="flex items-center">
+                <p className="font-mono">本数: </p>
+                <p className="ml-2 font-mono">{counts[index]}</p>
+              </div>
+              <div className="flex">
+                <button
+                  className="mr-1 p-2 font-mono bg-gray-300 rounded-lg"
+                  onClick={() => addCount(index)}
+                >
+                  +
+                </button>
+                <button
+                  className="ml-1 p-2 font-mono bg-gray-300 rounded-lg"
+                  onClick={() => reduceCount(index)}
+                >
+                  -
+                </button>
+              </div>
+              <button
+                className="px-4 py-2 font-mono bg-yellow-300 rounded-lg"
+                onClick={() => handleOnOrder(data.id, counts[index])}
+              >
+                送信
+              </button>
+              <Toaster />
+            </div>
           </div>
           <dl className="sm: flex flex-wrap justify-around p-7 w-1/2 text-gray-700 font-mono tracking-wide bg-yellow-50 rounded-lg sm:mt-4 sm:w-11/12">
             <dt className="flex w-3/12 font-bold leading-relaxed">
--- src/components/FrontWineLists/index.tsx
+++ src/components/FrontWineLists/index.tsx
@@ -1,9 +1,11 @@
 import { Data, Props } from "src/types";
 import { Toaster } from "react-hot-toast";
 import { handleDelete, handleOnOrder } from "src/method";
+import { useEffect, useState } from "react";
 
 export const FrontWineLists: React.FC<Props> = (props) => {
   const contents = props.contents;
+  const [counts, setCounts] = useState<number[]>([]);
 
   const rankData = contents?.filter(
     (data: Data) =>
@@ -18,9 +20,27 @@ export const FrontWineLists: React.FC<Props> = (props) => {
     );
   }
 
+  useEffect(() => {
+    for (let i = 0; i < rankData.length; i++) {
+      setCounts((count) => [...count, 0]);
+    }
+  }, []);
+
+  const addCount = (index: number) => {
+    setCounts(counts.map((count, i) => (i === index ? count + 1 : count)));
+  };
+
+  const reduceCount = (index: number) => {
+    if (counts[index] > 0) {
+      setCounts(counts.map((count, i) => (i === index ? count - 1 : count)));
+    } else {
+      return;
+    }
+  };
+
   return (
     <div className="h-screen">
-      {rankData?.map((data: Data) => (
+      {rankData?.map((data: Data, index) => (
         <div
           className="flex items-center justify-around mt-6 h-1/3 bg-blue-100 sm:flex-wrap sm:mx-auto sm:py-8 sm:w-11/12 sm:h-auto sm:rounded-lg"
           key={data.id}
@@ -37,13 +57,33 @@ export const FrontWineLists: React.FC<Props> = (props) => {
             >
               削除
             </button>
-            <button
-              className="mt-5 mx-3 py-2 w-1/3 font-mono bg-yellow-100 rounded-lg"
-              onClick={() => handleOnOrder(data.id)}
-            >
-              発注する
-            </button>
-            <Toaster />
+            <div className="flex justify-between mt-5">
+              <div className="flex items-center">
+                <p className="font-mono">本数: </p>
+                <p className="ml-2 font-mono">{counts[index]}</p>
+              </div>
+              <div className="flex">
+                <button
+                  className="mr-1 p-2 font-mono bg-gray-300 rounded-lg"
+                  onClick={() => addCount(index)}
+                >
+                  +
+                </button>
+                <button
+                  className="ml-1 p-2 font-mono bg-gray-300 rounded-lg"
+                  onClick={() => reduceCount(index)}
+                >
+                  -
+                </button>
+              </div>
+              <button
+                className="px-4 py-2 font-mono bg-yellow-300 rounded-lg"
+                onClick={() => handleOnOrder(data.id, counts[index])}
+              >
+                送信
+              </button>
+              <Toaster />
+            </div>
           </div>
           <dl className="sm: flex flex-wrap justify-around p-7 w-1/2 text-gray-700 font-mono tracking-wide bg-yellow-50 rounded-lg sm:mt-4 sm:w-11/12">
             <dt className="flex w-3/12 font-bold leading-relaxed">
