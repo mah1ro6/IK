51c3fdc44b6afb3b0a1ff42a33a45d3f7e700013
mahiro miyazaki <mahiro.siawasemono@ezweb.ne.jp>
2022-03-26 16:41:05 +0900

    ⚰️ 不要なコードの削除

--- src/pages/api/proxy/[...all].ts
+++ src/pages/api/proxy/[...all].ts
@@ -8,14 +8,9 @@ export const config = {
   },
 };
 
-const isDevelopment = process.env.NODE_ENV !== "production";
-
 const postRequest = async (req: NextApiRequest, res: NextApiResponse) => {
   await httpProxyMiddleware(req, res, {
-    target:
-      process.env.NODE_ENV === "development"
-        ? process.env.DEV_API_URL
-        : process.env.NEXT_PUBLIC_PRO_API_URL,
+    target: process.env.API_URL,
     changeOrigin: true,
     pathRewrite: [
       {
@@ -48,39 +43,5 @@ const postRequest = async (req: NextApiRequest, res: NextApiResponse) => {
       },
     ],
   });
-  // : await httpProxyMiddleware(req, res, {
-  //     target: process.env.PRO_API_URL,
-  //     changeOrigin: true,
-  //     pathRewrite: [
-  //       {
-  //         patternStr: "^/api/proxy/delete",
-  //         replaceStr: "/ik_request/api/delete.php",
-  //       },
-  //       {
-  //         patternStr: "^/api/proxy/addToFrontPost",
-  //         replaceStr: "/ik_request/api/addToFront.php",
-  //       },
-  //       {
-  //         patternStr: "^/api/proxy/frontBottlePost",
-  //         replaceStr: "/ik_request/api/reduceFrontBottle.php",
-  //       },
-  //       {
-  //         patternStr: "^/api/proxy/onOrderPost",
-  //         replaceStr: "/ik_request/api/onOrder.php",
-  //       },
-  //       {
-  //         patternStr: "^/api/proxy/offOrderPost",
-  //         replaceStr: "/ik_request/api/offOrder.php",
-  //       },
-  //       {
-  //         patternStr: "^/api/proxy/addToCellarPost",
-  //         replaceStr: "/ik_request/api/addToCellar.php",
-  //       },
-  //       {
-  //         patternStr: "^/api/proxy/setItemTextPost",
-  //         replaceStr: "/ik_request/api/setItemText.php",
-  //       },
-  //     ],
-  //   });
 };
 export default postRequest;
